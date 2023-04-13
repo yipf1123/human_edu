b6389938f98268a1c71abb40f5c44fe2ea786bd9
yipf1123 <les805@naver.com>
2023-04-13 11:09:56 +0900

    updated

---
+++ Java/Java/src/Day01/Ex01_print.java
@@ -0,0 +1,14 @@
+package Day01;
+
+import javax.swing.event.SwingPropertyChangeSupport;
+
+public class Ex01_print {
+    public static void main(String[] args) {
+        System.out.print("안녕하세요 출력\n");
+        System.out.println("안녕하세요 출력 후 줄바꿈");
+
+        System.out.println("자바 첫 수업...");
+        System.out.println();
+        System.out.println("이어지는 문자열...");
+    }
+}
